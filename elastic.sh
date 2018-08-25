325c35819b7e7337b2b51e4271af0f2f01649261
ramnamdas <ram.nettech@gmail.com>
2018-08-25 17:14:00 +0530

    Update elastic.sh

--- elastic.sh
+++ elastic.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-yum update
+sudo yum update
 java -version > /dev/null 2>&1
 if [ `echo $?` -ne 0 ]
 then
@@ -7,15 +7,14 @@ then
        sleep 20
 fi
 echo "Installing Elasticsearch"
-echo '''[elasticsearch-2.x]
+sudo echo '''[elasticsearch-2.x]
 name=Elasticsearch repository for 2.x packages
 baseurl=http://packages.elastic.co/elasticsearch/2.x/centos
 gpgcheck=1
 gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
 enabled=1
-''' |  tee /etc/yum.repos.d/elasticsearch.repo
-yum -y install elasticsearch
-sleep 10
-sed -i 's/# network.host: 192.168.0.1/network.host: localhost/g' /etc/elasticsearch/elasticsearch.yml
-service elasticsearch start
-service elasticsearch status
+''' |sudo tee /etc/yum.repos.d/elasticsearch.repo
+sudo yum -y install elasticsearch
+sudo sed -i 's/# network.host: 192.168.0.1/network.host: localhost/g' /etc/elasticsearch/elasticsearch.yml
+sudo service elasticsearch start
+sudo service elasticsearch status
