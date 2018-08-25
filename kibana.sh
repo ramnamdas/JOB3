18d0a019e290481d979f43bead9e2b443cf8769e
ramnamdas <ram.nettech@gmail.com>
2018-08-25 16:21:27 +0530

    Create kibana.sh

---
+++ kibana.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+yum update
+java -version > /dev/null 2>&1
+if [ `echo $?` -ne 0 ]
+then
+        yum install java -y
+       sleep 20
+fi
+echo "Installing Kibana"
+echo ''' [kibana-4.4]
+name=Kibana repository for 4.4.x packages
+baseurl=http://packages.elastic.co/kibana/4.4/centos
+gpgcheck=1
+gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+''' |  tee /etc/yum.repos.d/kibana.repo
+yum -y install kibana
+sleep 10
+sed -i 's/# server.host: "0.0.0.0"/server.host: "0.0.0.0"/g' /opt/kibana/config/kibana.yml
+service kibana start
+service kibana status
