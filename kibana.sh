8d2290dde48e96f955a051fd8b2f13847783d555
ramnamdas <ram.nettech@gmail.com>
2018-08-27 00:28:23 +0530

    Update kibana.sh

--- kibana.sh
+++ kibana.sh
@@ -17,8 +17,4 @@ enabled=1
 ''' | sudo  tee /etc/yum.repos.d/kibana.repo
 sudo yum -y install kibana
 sudo sed -i "s/# server.host: "0.0.0.0"/server.host: "0.0.0.0"/g" /opt/kibana/config/kibana.yml
-echo "
-elasticsearch.url: \"http://${om}:9200\"
-" | sudo tee -a /opt/kibana/config/kibana.yml
-sudo service kibana start
-sudo service kibana status
+sudo sed -i "s/# elasticsearch.url: \"http:\/\/localhost:9200\"/elasticsearch.url: \"http:\/\/${om}:9200\"/g" /opt/kibana/config/kibana.yml
